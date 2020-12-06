fa2b1ac28d0e3ae8778d8df983f6600ea8d5dbef
MohamedAli Mahdhaoui <med_ali_mahdhaoui@yahoo.fr>
2020-12-06 17:17:39 +0100

    new code from scratch without style

--- src/App.js
+++ src/App.js
@@ -6,50 +6,51 @@ class App extends React.Component {
     super(props);
     this.state = {
       Person: {
-        fullName: "Mohamed Ali",
-        bio: "Here is my bio",
-        imgSrc: "/photo.jpg",
+        name: "Mohamed ",
+        age: "28",
         profession: "engineer",
+        imgSrc: "/photo.jpg",
       },
-      toggle: false,
+      x: false,
     };
   }
 
-  handleClick = () =>
-    this.state.toggle
-      ? this.setState({ toggle: false })
-      : this.setState({ toggle: true });
+  clickHandler = () => {
+    let { x } = this.state;
+    x ? this.setState({ x: false }) : this.setState({ x: true });
+  };
 
-  incrementCounter = () => {
-    const { counter } = this.state;
-    this.state.toggle
-      ? this.setState({ counter: counter + 1 })
-      : this.setState({ counter: 0 });
+  timeCount = () => {
+    const { timer } = this.state;
+    this.state.x
+      ? this.setState({ timer: timer + 1 })
+      : this.setState({ timer: 0 });
   };
 
   componentDidMount = () => {
-    setInterval(this.incrementCounter, 1000);
+    setInterval(this.timeCount, 1000);
   };
 
   render() {
     return (
-      <div>
-        <p>{this.state.counter}</p>
-        <button onClick={this.handleClick}>
-          {this.state.toggle ? "Hide" : "show"}
+      <div className="App">
+        <button onClick={this.clickHandler}>
+          {this.state.x === true ? <p>Hide</p> : <p>Show</p>}
         </button>
-        {this.state.toggle && (
-          <div className="App">
-            <div className="ImgSide">
-              <img src={this.state.Person.imgSrc} />
-            </div>
-            <div className="rightSide">
-              <div>{this.state.Person.fullName}</div>
-              <div>{this.state.Person.bio}</div>
-              <div>{this.state.Person.profession}</div>
-            </div>
-          </div>
-        )}
+        {this.state.x
+          ? Object.keys(this.state.Person).map((key) =>
+              key !== "imgSrc" ? (
+                <div>{key + ":" + this.state.Person[key]}</div>
+              ) : (
+                <img
+                  src={this.state.Person.imgSrc}
+                  style={{ width: "400px" }}
+                  alt="profile img"
+                />
+              )
+            )
+          : ""}
+        <div>{this.state.timer}</div>
       </div>
     );
   }
